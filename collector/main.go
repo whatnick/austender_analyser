88fd674c496a9072670cccfe0a34445368747ab3
Tisham (whatnick) Dhar <tisham@whatnick.com>
2023-01-25 21:58:20 +1030

    Get all the contracts

--- collector/main.go
+++ collector/main.go
@@ -12,8 +12,31 @@ import (
 	"github.com/gocolly/colly"
 )
 
+// Fields
+/*
+	CN ID:CN3482539-A2
+	Amends:CN3482539
+	Agency:Australian National Audit Office
+	Publish Date:6-Feb-2018
+	Category:Audit services
+	Contract Period:22-Jan-2018 to 31-Oct-2023
+	Contract Value (AUD):$542,560.00
+	ATM ID:2017/1102
+	Supplier Name:KPMG Peat Marwick - ACT
+*/
+type contract struct {
+	CN_ID           string
+	Amends          string
+	Agency          string
+	Publish_Date    string
+	Category        string
+	Contract_Period string
+	Contract_Value  string
+	ATM_ID          string
+	Supplier_Name   string
+}
+
 func main() {
-	client := &http.Client{}
 	collector := colly.NewCollector()
 	companyName := "KPMG"
 	params := url.Values{}
@@ -24,6 +47,49 @@ func main() {
 	params.Add("DateType", "Publish Date")
 	params.Add("SupplierName", companyName)
 	requestURL := "https://www.tenders.gov.au/Search/CnAdvancedSearch?" + params.Encode()
+
+	// FIXME: No more need for initial load with Colly
+	//initialLoad(requestURL)
+
+	collector.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		url := e.Attr("href")
+		if strings.Contains(url, "SupplierName="+companyName) {
+			//fmt.Printf(url)
+			// Visit all search bread crumbs
+			e.Request.Visit(url)
+		}
+	})
+
+	collector.OnHTML(".col-sm-8", func(e *colly.HTMLElement) {
+		c := &contract{}
+		e.ForEach(".list-desc", func(_ int, el *colly.HTMLElement) {
+			switch el.ChildText("span") {
+			case "CN ID:":
+				c.CN_ID = el.ChildText(".list-desc-inner")
+			case "Agency:":
+				c.Agency = el.ChildText(".list-desc-inner")
+			case "Publish Date:":
+				c.Publish_Date = el.ChildText(".list-desc-inner")
+			case "Category:":
+				c.Category = el.ChildText(".list-desc-inner")
+			case "Contract Period:":
+				c.Contract_Period = el.ChildText(".list-desc-inner")
+			case "Contract Value (AUD):":
+				c.Contract_Value = el.ChildText(".list-desc-inner")
+			case "ATM ID:":
+				c.ATM_ID = el.ChildText(".list-desc-inner")
+			case "Supplier Name:":
+				c.Supplier_Name = el.ChildText(".list-desc-inner")
+			}
+		})
+		fmt.Printf(c.Contract_Value)
+	})
+
+	collector.Visit(requestURL)
+}
+
+func initialLoad(requestURL string) {
+	client := &http.Client{}
 	req, err := http.NewRequest("GET", requestURL, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -54,34 +120,11 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("%s\n", bodyText)
-
-	collector.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		url := e.Attr("href")
-		if strings.Contains(url, "SupplierName="+companyName) {
-			fmt.Printf(url)
-			// Visit all search bread crumbs
-			e.Request.Visit(url)
-		}
-	})
-
-	collector.Visit(requestURL)
 }
 
 // TODO: Create contract class and return a list
 // of contracts/tenders per page
 func summarizePage(s string) *list.List {
-	// Fields
-	/*
-		CN ID:CN3482539-A2
-		Amends:CN3482539
-		Agency:Australian National Audit Office
-		Publish Date:6-Feb-2018
-		Category:Audit services
-		Contract Period:22-Jan-2018 to 31-Oct-2023
-		Contract Value (AUD):$542,560.00
-		ATM ID:2017/1102
-		Supplier Name:KPMG Peat Marwick - ACT
-	*/
 	l := list.New()
 	return l
 }
